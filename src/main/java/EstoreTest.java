8165231f1dd8ada4919d0a7689699580722dcc85
zhaolg <zhaolg@shpso.com>
2017-12-28 19:08:28 +0800

    modif by zhaolg

---
+++ src/main/java/EstoreTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zhaolg on 2017/12/28
+ * @return
+ * @throws Exception
+ */
+public class EstoreTest {
+
+    public static void main(String [] agrs){
+                
+
+    }
+
+}
